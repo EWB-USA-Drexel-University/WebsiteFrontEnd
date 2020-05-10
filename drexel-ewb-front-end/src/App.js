457acd0d24c0aea35c556ef371b0deef15a3bdde
Nathan Beebe <nbeebe8@gmail.com>
2020-05-09 22:23:50 -0400

    Prototype Navigation Bar

--- drexel-ewb-front-end/src/App.js
+++ drexel-ewb-front-end/src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='main'>
+            <nav className='mainNav'>
+                <a className='link' href='home'>Home</a>
+                <a className='link' href='about_us'>About Us</a>
+                <a className='link' href='our_projects'>Our Projects</a>
+                <a className='link' href='get_involved'>Get Involved</a>
+                <a className='link' href='meet_the_team'>Meet The Team</a>
+            </nav>
+            <p>Engineers Without Borders Home</p>
+        </div>
+    );
 }
+
+
+
+
+
 
 export default App;
