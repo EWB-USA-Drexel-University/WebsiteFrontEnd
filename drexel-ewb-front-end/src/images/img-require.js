3cb57aab67743f3f128a86eeb4669f0e68dec5a6
Nathan Beebe <nbeebe8@gmail.com>
2020-07-15 22:48:57 -0400

    Font, Meet Team

---
+++ drexel-ewb-front-end/src/images/img-require.js
@@ -0,0 +1,9 @@
+
+const images = {
+    eboard: {
+        gm: require('./eboard/gavin-macguire.jpeg'),
+        mf: require('./eboard/malena-farber.jpg')
+    }
+};
+
+export default images;
--- drexel-ewb-front-end/src/index.js
+++ drexel-ewb-front-end/src/index.js
@@ -3,16 +3,20 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import './info-sections.css'
 import content from './home.json'
+import images from "./images/img-require";
 import { Slide } from 'react-slideshow-image'
 import 'react-slideshow-image/dist/styles.css'
 import {
     BrowserRouter as Router,
     Switch,
     Route,
-    Link
+    Link,
+    useRouteMatch,
+    useParams
 } from "react-router-dom";
 
 export default class App extends React.Component {
+
     render() {
         return (
             <div className='starter'>
@@ -83,19 +87,19 @@ export default class App extends React.Component {
                     <Slide {...properties}>
                         <div className='each-slide'>
                             <div className={'each-slide'}>
-                            <span><img src={require('./images/img1.jpg')} alt='sample photo'
+                            <span><img src={require('./images/img1.jpg')} alt='sample'
                                        width={"80%"} height={"100%"}/></span>
                             </div>
                         </div>
                         <div className='each-slide'>
                             <div className={'each-slide'}>
-                            <span><img src={require('./images/img2.jpg')} alt='sample photo'
+                            <span><img src={require('./images/img2.jpg')} alt='sample'
                                        width={"100%"} height={"100%"}/></span>
                             </div>
                         </div>
                         <div className='each-slide'>
                             <div className={'each-slide'}>
-                            <span><img src={require('./images/img3.jpg')} alt='sample photo'
+                            <span><img src={require('./images/img3.jpg')} alt='sample'
                                        width={"100%"} height={"100%"}/></span>
                             </div>
                         </div>
@@ -122,16 +126,76 @@ export default class App extends React.Component {
     }
 
     projects() {
-        return <h2>Projects</h2>;
+
+        // let match = this.matchRoute();
+
+        return (
+            <div>
+                <h2>Projects</h2>
+                {/*    <ul>*/}
+                {/*        <li>*/}
+                {/*            <Link to={`${match.url}/project1`}>*/}
+                {/*                Project 1*/}
+                {/*            </Link>*/}
+                {/*        </li>*/}
+                {/*        <li>*/}
+                {/*            <Link to={`${match.url}/project2`}>*/}
+                {/*                Project 2*/}
+                {/*            </Link>*/}
+                {/*        </li>*/}
+                {/*    </ul>*/}
+
+                {/*<Switch>*/}
+                {/*    <Route path={`${match.path}/:projectId`}>*/}
+                {/*        {this.project()}*/}
+                {/*    </Route>*/}
+                {/*    <Route path={match.path}>*/}
+                {/*        <h3>Choose a project.</h3>*/}
+                {/*    </Route>*/}
+                {/*</Switch>*/}
+            </div>
+        );
     }
+
+    // project() {
+    //     let { projectId } = useParams();
+    //     return <h3>Requested project ID: {projectId}</h3>
+    // }
+
     get_involved() {
         return <h2>Get Involved</h2>;
     }
 
     meet_the_team() {
-        return <h2> Meet The Team</h2>;
+        return (
+            <div>
+                <h2>Meet The Team</h2>
+                <div className={'row'}>
+                    <p>{this.memberProfile('Sheila', 'President')}</p>
+                    <p>{this.memberProfile('Gavin Maguire', 'Sustainability Chair', images.eboard.gm)}</p>
+                    <p>{this.memberProfile('Michael', 'CFO')}</p>
+                </div>
+            </div>
+    );
     }
+
+    memberProfile(name, position, img) {
+
+        return(
+                <div className={'column'}>
+                    <div className="card">
+                        <img src={img} width={'100%'} height={'100%'} />
+                        <div className="container">
+                            <h2>{name}</h2>
+                            <p className="title">{position}</p>
+                        </div>
+                    </div>
+                </div>
+    );
+    }
+
 }
+
 
 ReactDOM.render(
     <App />,
