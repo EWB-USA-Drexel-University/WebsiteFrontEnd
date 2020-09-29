78b8373e4cc7a23c136de7af414ac2f7752978b4
Nathan Beebe <nbeebe8@gmail.com>
2020-09-28 21:35:51 -0400

    files moved up a level

---
+++ src/images/img-require.js
@@ -0,0 +1,36 @@
+
+const images = {
+    eboard: {
+        president: require('./eboard/pres-jill-saunders.jpg'),
+        vice_president: require('./eboard/vice_pres-josh-mcguckin.jpeg'),
+        secretary: require('./eboard/secr-marley-downes.jpg'),
+        marketing: require('./eboard/marketing-bronwyn-sayre.jpg'),
+        treasurer: require('./eboard/treasurer-jared-bunch.jpg'),
+        event_coord: require('./eboard/event_coordinator-betty-long.jpg'),
+        member_coord: require('./eboard/member_coordinator-katherine-comisac.jpg'),
+        global_relations_lead: require('./eboard/glob_rel_lead-will-scales.JPG'),
+        social_media: require('./eboard/social_media-sara-scanlin.JPG'),
+        sustainability_chair: require('./eboard/sustain_chair-gavin-macguire.jpeg'),
+        domestic_design_lead: require('./eboard/dom_design_lead-malena-farber.jpg'),
+        miramar_lead: require('./eboard/miramar_lead-isabella-snyder.jpeg'),
+        safac_chair: require('./eboard/safac_chair-youssef-jouchiate.png'),
+        travel_coord: require('./eboard/travel_team_coord-darrell-omo-lamai.jpeg'),
+        fundraising_lead: require('./eboard/fundraising-patty-inroon.jpeg'),
+        tech_lead: require('./eboard/tech_lead-har-patel.jpg'),
+        grid_lead: require('./eboard/grid_lead-maggie-mccurdy.jpg'),
+        ecuador_lead: require('./eboard/ecuador_lead-nicole-tavormina.jpg')
+    },
+    logo: {
+        navLogo: require('./globe-logo.png'),
+        DU_EWB: require('./Drexel University Chapter_1Color.png'),
+        DU_EWB_White: require('./du_ewb_whitelogo.png')
+    },
+    content: {
+        gbm_image: require('./GBM-Picture.JPG'),
+        meet_n_greet: require('./meet_and_greet.jpg'),
+        miramar_cover: require('./miramar_cover.JPG'),
+        ecuador_cover: require('./ecuador_cover.jpg')
+    }
+};
+
+export default images;
