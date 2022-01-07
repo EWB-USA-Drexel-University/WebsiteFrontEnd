902c6bb26729e67a433ccc96f07eab81c5ecc628
Nathan Beebe <nbeebe8@gmail.com>
2022-01-07 17:59:35 -0500

    Updated year in footer

--- src/index.js
+++ src/index.js
@@ -337,7 +337,7 @@ export default function App() {
                 </Router>
                 <footer>
                     <div className={'footer'} style={{ textAlign: 'center' }}>
-                        <h5>&#169; Engineers Without Borders: Drexel University 2021</h5>
+                        <h5>&#169; Engineers Without Borders: Drexel University 2022</h5>
                         <p><a href='https://github.com/EWB-USA-Drexel-University/WebsiteFrontEnd'>GitHub</a></p>
                     </div>
                 </footer>
