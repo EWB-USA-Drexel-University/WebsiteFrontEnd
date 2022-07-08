31c76bd8847c1e5590cea8aee519f0e50989d3ca
Nathan Beebe <njbeebe@outlook.com>
2022-07-07 20:41:09 -0400

    moved AboutUs into its very own file!

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import {
     Route,
     Link
 } from "react-router-dom";
+import AboutUs from './components/AboutUs';
 
 export default function App() {
 
@@ -112,60 +113,6 @@ export default function App() {
             {infoSection(content.home.mission_header, content.home.mission_body)}
             {big_info()}
         </div>
-        );
-    }
-
-
-    function about() {
-
-        function memberProfile(name, position, img) {
-
-            return (
-                <div className={'column'}>
-                    <div className="card">
-                        <img src={img} className={'team-image'} />
-                        <div className="container">
-                            <h2>{name}</h2>
-                            <p className="title">{position}</p>
-                        </div>
-                    </div>
-                </div>
-            );
-        }
-
-        function meet_the_team() {
-            return (
-                <div className='team-content'>
-                    <h2>Meet The Team</h2>
-                    <div className={'row'}>
-                        <p>{memberProfile('Jillian Saunders', 'President', images.eboard.president)}</p>
-                        <p>{memberProfile('Bronwyn Sayre', 'Vice President', images.eboard.vice_president)}</p>
-                        <p>{memberProfile('Darrell Omo-Lamai', 'Treasurer', images.eboard.treasurer)}</p>
-                        <p>{memberProfile('Ned Elshafi', 'Event Coordinator', images.eboard.event_coord)}</p>
-                        <p>{memberProfile('Eva Kraus', 'Secretary', images.eboard.secretary)}</p>
-                        <p>{memberProfile('Marley Downes', 'GRID Lead', images.eboard.grid_lead)}</p>
-                        <p>{memberProfile('Kate Comisac', 'Domestic Design Lead', images.eboard.domestic_design_lead)}</p>
-                        <p>{memberProfile('Patty Inroon', 'Fundraising Lead', images.eboard.fundraising_lead)}</p>
-                        <p>{memberProfile('Amanda Milo', 'Marketing Lead', images.eboard.marketing)}</p>
-                        <p>{memberProfile('Nathan Beebe', 'Tech Lead', images.eboard.tech_lead)}</p>
-                        <p>{memberProfile('Sukethram Sivakumar', 'Member Coordinator', images.eboard.member_coord)}</p>
-
-                        <p>{memberProfile('Will Scales', 'Global Relations Chair', images.chairs.global_relations_lead)}</p>
-                        <p>{memberProfile('Nicole Tavormina', 'Ecuador Lead', images.chairs.san_luis_lead)}</p>
-                        <p>{memberProfile('Elizabeth Clarkson', 'Sustainability Lead', images.chairs.sustainability_chair)}</p>
-                        <p>{memberProfile('Alyssa Kemp', 'Travel Coordinator', images.chairs.travel_coord)}</p>
-                        <p>{memberProfile('Justin Stiles', 'Project Finder', images.chairs.project_finder)}</p>
-
-                    </div>
-                </div>
-            );
-        }
-
-        return (
-            <div>
-                {infoSection(content.about.about_header, content.about.about_summary)}
-                {meet_the_team()}
-            </div>
         );
     }
 
@@ -322,7 +269,7 @@ export default function App() {
                         {logo_color()}
                         <Switch>
                             <Route path={'/about'}>
-                                {about()}
+                                <AboutUs />
                             </Route>
                             <Route path={'/projects'}>
                                 {projects()}
