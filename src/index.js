8ecd30a856602b3b72a161a110106b5acee16301
Yonatan <yw692@drexel.edu>
2022-07-14 20:56:13 -0400

    Get Involved Component Created

--- src/index.js
+++ src/index.js
@@ -4,19 +4,17 @@ import './index.css'
 import './info-sections.css'
 import content from './content/content.json';
 import images from "./images/img-require";
-import MailchimpSubscribe from "react-mailchimp-subscribe";
+
 import ReactGA from 'react-ga';
-import 'react-slideshow-image/dist/styles.css'
 import {
     HashRouter as Router,
     Switch,
     Route,
     Link
 } from "react-router-dom";
-import AboutUs from './components/about-us/AboutUs';
-
-
-import {HomePage} from './components/home-page/HomePage.js'
+import AboutUs from './components/about-us/AboutUs'
+import {HomePage} from './components/home-page/HomePage'
+import GetInvolved from './components/get-involved/GetInvolved'
 
 export default function App() {
 
@@ -33,19 +31,6 @@ export default function App() {
         }
     }
 
-
-    function infoSection(header, body) {
-        return (
-            <div className='section-container'>
-                <div className='info-section'>
-                    <div className='info-section-header'>
-                        <h3 className='section-header'>{header}</h3>
-                    </div>
-                    <p className='section-body'>{body}</p>
-                </div>
-            </div>
-        );
-    }
 
 
     function projects() {
@@ -93,55 +78,6 @@ export default function App() {
             </div>
         );
 
-    }
-
-    function mailchimp() {
-
-        const url = 'https://drexelewb.us17.list-manage.com/subscribe/post?u=eb498d0265dc0ed85f1f7d71e&amp;id=c6b77ac831';
-
-        return (
-            <div className={'mail-form'}>
-                <div className={'mail-header'}>
-                    <h3>News Letter</h3>
-                </div>
-                <p>{content.get_involved.news_letter.news_letter_desc}</p>
-                <MailchimpSubscribe url={url} />
-            </div>
-
-        );
-    }
-
-
-
-    function get_involved() {
-        return (
-            <div>
-                <div className={"page-header"}>
-                    <h2>Get Involved</h2>
-                    {mailchimp()}
-                </div>
-                <p>
-                    <div className={'content-img-container'}>
-                        <img src={images.content.domestic} className='project-image' />
-                    </div>
-                    {infoSection(content.get_involved.domestic_design.domestic_header,
-                        content.get_involved.domestic_design.domestic_info)}
-                    <div className={'content-img-container'}>
-                        <img src={images.content.grid} className='project-image' />
-                    </div>
-                    {infoSection(content.get_involved.grid.grid_header, content.get_involved.grid.grid_info)}
-                    <div className={'content-img-container'}>
-                        <img src={images.content.tech} className='project-image' />
-                    </div>
-                    {infoSection(content.get_involved.tech.tech_header, content.get_involved.tech.tech_info)}
-                    <div className={'content-img-container'}>
-                        <img src={images.content.meet_n_greet} className='project-image' />
-                    </div>
-                    {infoSection(content.get_involved.marketing_fundraising.marketing_header,
-                        content.get_involved.marketing_fundraising.marketing_info)}
-                </p>
-            </div>
-        );
     }
 
     const [darkMode, setDarkMode] = React.useState(getInitialMode());
@@ -207,7 +143,7 @@ export default function App() {
                                 {projects()}
                             </Route>
                             <Route path={'/get_involved'}>
-                                {get_involved()}
+                                <GetInvolved />
                             </Route>
                             <Route path={'/'}>
                                 <HomePage />
