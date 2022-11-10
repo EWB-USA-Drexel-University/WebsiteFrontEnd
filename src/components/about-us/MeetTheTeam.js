5073e35672b2561e26a9bbbf95f14267ff3daa9e
Nathan Beebe <njbeebe@outlook.com>
2022-11-09 22:33:27 -0500

    Updated Suketh titles

--- src/components/about-us/MeetTheTeam.js
+++ src/components/about-us/MeetTheTeam.js
@@ -25,11 +25,10 @@ export default function MeetTheTeam() {
                 <div className={'row'}>
                     <p>{memberProfile('Bronwyn Sayre', 'President', images.eboard.president)}</p>
                     <p>{memberProfile('Kate Comisac', 'Vice President', images.eboard.vice_president)}</p>
-                    <p>{memberProfile('Suketh Sivakumar', 'Treasurer', images.eboard.treasurer)}</p>
+                    <p>{memberProfile('Suketh Sivakumar', 'Treasurer, Fundraising Lead', images.eboard.treasurer)}</p>
                     <p>{memberProfile('Chris Bahr', 'Secretary', images.eboard.secretary)}</p>
                     <p>{memberProfile('Eva Kraus', 'GRID Lead', images.eboard.grid_lead)}</p>
                     <p>{memberProfile('Michael Luciano', 'Domestic Design Lead', images.eboard.domestic_design_lead)}</p>
-                    <p>{memberProfile('Sukethram Sivakumar', 'Fundraising Lead', images.eboard.fundraising_lead)}</p>
                     <p>{memberProfile('Amanda Milo', 'Marketing Lead', images.eboard.marketing)}</p>
                     <p>{memberProfile('Yonatan Wiese-Namir', 'Tech Lead', images.eboard.tech_lead)}</p>
                     <p>{memberProfile('Sofia Martinez', 'Member Coordinator', images.eboard.member_coord)}</p>
